45a0935a33468f6b050bc4e4b0609ce0e8e0e8fa
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-08-14 08:17:01 -0400

    update

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+    let sum = 0;
+
+
+let sumForEach = function(arr){
+    let num = arr.forEach();
+    sum += num;
+    return sum;
+
+
+
+}
+
+
+
+console.log(sumForEach([6,5])); // 0
+console.log(sumForEach([4,])); 
+console.log(sumForEach([6, 7, 2])); // 15
